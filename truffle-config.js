32787912dd0a17ca2de224111a17765a896eb4a6
divyam <divyamsingh13@gmail.com>
2018-02-28 19:17:02 +0530

    Added Controllers

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,32 @@
+var DefaultBuilder = require("truffle-default-builder");
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    build: new DefaultBuilder({
+        "index.html": "index.html",
+        "app.js": [
+            "vendor/angular/angular.js",
+            "vendor/angular-route/angular-route.js",
+            "javascript/app.js",
+
+
+        ],
+        "app.css": [
+            "stylesheets/app.css"
+        ],
+
+        "views/": "views/"
+    }),
+
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "1337" // match any network
+        },
+        live: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*" // match any network
+        }
+    }
 };
